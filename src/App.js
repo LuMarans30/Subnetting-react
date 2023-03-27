9b0b217d0c840e7cedf38b95bba9c08af21b9aa0
LuMarans30 <andreamarano2004@gmail.com>
2023-03-27 23:11:05 +0200

    better style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Card, CardContent  } from '@mui/material';
-import { LightMode, DarkMode } from '@mui/icons-material/';
+import { Card, CardContent, Stack } from '@mui/material';
+import { LightMode, DarkMode, Margin, Padding } from '@mui/icons-material/';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { IconButton } from '@mui/material';
+
+import { PaletteSwitch } from '@trautmann/theme-switch';
 
 import SubnetForm from './SubnetForm';
 import React from 'react';
@@ -25,6 +27,32 @@ function App() {
   const [mode, setMode] = React.useState('dark');
 
   const theme = createTheme({
+    transitions: {
+      duration: {
+        shortest: 450,
+        shorter: 500,
+        short: 550,
+        // most basic recommended timing
+        standard: 600,
+        // this is to be used in complex animations
+        complex: 875,
+        // recommended when something is entering screen
+        enteringScreen: 525,
+        // recommended when something is leaving screen
+        leavingScreen: 495,
+      },
+      easing: {
+        // This is the most common easing curve.
+        easeInOut: 'cubic-bezier(0.4, 0, 0.2, 1)',
+        // Objects enter the screen at full velocity from off-screen and
+        // slowly decelerate to a resting point.
+        easeOut: 'cubic-bezier(0.0, 0, 0.2, 1)',
+        // Objects leave the screen at full velocity. They do not decelerate when off-screen.
+        easeIn: 'cubic-bezier(0.4, 0, 1, 1)',
+        // The sharp curve is used by objects that may return to the screen at any time.
+        sharp: 'cubic-bezier(0.4, 0, 0.6, 1)',
+      },
+    },
     palette: {
       mode: mode,
     }
@@ -45,7 +73,7 @@ function App() {
     }
   }
 
-  React.useMemo( () =>
+  React.useMemo(() =>
     setMode(prefersDarkMode ? 'dark' : 'light'),
     [prefersDarkMode]
   );
@@ -59,16 +87,10 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <IconButton onClick={() => {
-        mode === 'dark' ? setMode('light') : setMode('dark');
-        console.log(theme);
-      }}>
-        {mode === 'dark' ? (<LightMode />) : (<DarkMode />)}
-      </IconButton>
-
       <div id={gradient}>
         <Card className='Card' variant="outlined" style={styles.card}>
           <CardContent>
+            <PaletteSwitch size="large" onChange={setMode} value={mode} />
             <SubnetForm
               OnSubmit={(ipaddr, numhost) => {
                 subnetCalc.addSubnet(ipaddr, numhost);
